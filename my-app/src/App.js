1c4323df727de5a72bc2aebafb1c142141238191
Evan Oyster <oyster.evan@gmail.com>
2023-01-29 09:04:29 -0500

    removed tutorial examples

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,21 +1,13 @@
-import React, {useState, useEffect} from 'react';
+import React from 'react';
 import Nav from './Components/Nav'
 import Header from './Components/Header'
-import axios from 'axios';
+
 
 
 function App() {
-  const [name, setName] = useState("unknown");
-
-  useEffect(() =>{
-    axios.get('/api/name').then(response => {
-      setName(response.data.name)
-    })
-  }, [])
 
   return (
     <div className="App">
-      <p>{name} is the best</p>
       <Header />
       <Nav />
     </div>
