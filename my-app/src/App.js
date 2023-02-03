b5eedf0cff7b2359cf344140f9df378ce6466965
Evan Oyster <oyster.evan@gmail.com>
2023-02-03 10:12:18 -0500

    starting search api

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,8 +2,6 @@ import React from 'react';
 import Nav from './Components/Nav'
 import Header from './Components/Header'
 
-
-
 function App() {
 
   return (
--- my-app/src/Components/DisplaySearch.js
+++ my-app/src/Components/DisplaySearch.js
@@ -1,9 +1,23 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 function DisplaySearch({tweetSearching}) {
 
+  // const [url, setUrl] = useState();  
+
+  useEffect(() => {
+    const getTweet = async () => {
+      const response = await axios.get(`https://api.twitter.com/1.1/search/tweets.json?q=${tweetSearching}`);
+      const tweetID = response.status.id;
+      return console.log(tweetID)
+
+    }
+    getTweet();
+
+  }, [])
+
     return (
-      <div>{tweetSearching}</div>
+      <div></div>
     )
   }
 
