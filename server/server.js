dc389a241f539b23eb59d3ab2147ef7a0cde310a
Evan Oyster <oyster.evan@gmail.com>
2023-02-07 11:52:03 -0500

    jwt installed

--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const axios = require('axios');
+const jwt = require('jsonwebtoken');
 const port = 3001;
 
 app.get('/', (req, res) => {
