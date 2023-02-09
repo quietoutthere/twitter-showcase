7db807ebb02cb73ad935bd1a7f55c67550e7aa8f
Evan Oyster <oyster.evan@gmail.com>
2023-02-09 10:41:50 -0500

    env installed

--- server/server.js
+++ server/server.js
@@ -1,20 +1,25 @@
 const express = require('express');
 const app = express();
-const jwt = require('jsonwebtoken');
+const axios = require('axios');
 const port = 3001;
 
-app.get('/', (req, res) => {
-  res.send('Hello Evan!')
-});
+require('dotenv').config();
 
-app.get('/api/name', (req, res) => {
-    const response = {
-        name: 'Evan'
-    }
-    
+app.get('/', (req, res) => {
+    const auth = 'AAAAAAAAAAAAAAAAAAAAAK4mlgEAAAAAW7D%2Briplp2Sy9h%2Bf4BqGEC5iXuE%3D0PEeKstHIrFwEJpHcZSR88htZOIBqJgLBHTAzF3b2FFvhmaVvN'
+    const response = axios({
+      method: 'get',
+      url: 'https://api.twitter.com/1.1/search/tweets.json?q=lexfridman',
+      headers: {
+        "bearer": auth,
+      }
+
+    })
     res.send(response)
 });
 
+ 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 });
+
