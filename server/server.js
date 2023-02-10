8e4327a3292c6ab610fb1192c5740cea523e3ce1
Evan Oyster <oyster.evan@gmail.com>
2023-02-10 10:01:31 -0500

    receiving response.data in terminal

--- server/server.js
+++ server/server.js
@@ -3,19 +3,30 @@ const app = express();
 const axios = require('axios');
 const port = 3001;
 
-require('dotenv').config();
+require('dotenv').config()
+
+const token = process.env.BEARER_TOKEN;
+let baseUrl = 'https://api.twitter.com/1.1/search/tweets.json?q=lexfridman';
+
+console.log(token);
 
 app.get('/', (req, res) => {
-    const auth = 'AAAAAAAAAAAAAAAAAAAAAK4mlgEAAAAAW7D%2Briplp2Sy9h%2Bf4BqGEC5iXuE%3D0PEeKstHIrFwEJpHcZSR88htZOIBqJgLBHTAzF3b2FFvhmaVvN'
-    const response = axios({
-      method: 'get',
-      url: 'https://api.twitter.com/1.1/search/tweets.json?q=lexfridman',
-      headers: {
-        "bearer": auth,
-      }
-
-    })
-    res.send(response)
+  
+  const authAxios = axios.create({
+    baseUrl: baseUrl,
+    headers: {
+      Authorization: `Bearer ${token}`
+    }
+  })
+  
+  authAxios.get(baseUrl) 
+   .then((response) => {
+    console.log(response.data.statuses);
+    res.send(response.data.statuses);
+   }) .catch((error) => {
+    console.log(error)
+   })
+  
 });
 
  
