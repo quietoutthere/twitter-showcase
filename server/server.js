57682d96cc437143626672d73e0a05d7733d4fa4
Evan Oyster <oyster.evan@gmail.com>
2023-02-12 09:01:42 -0500

    env updated

--- server/server.js
+++ server/server.js
@@ -8,28 +8,24 @@ require('dotenv').config()
 const token = process.env.BEARER_TOKEN;
 let baseUrl = 'https://api.twitter.com/1.1/search/tweets.json?q=lexfridman';
 
-console.log(token);
-
 app.get('/', (req, res) => {
-  
   const authAxios = axios.create({
     baseUrl: baseUrl,
     headers: {
       Authorization: `Bearer ${token}`
     }
   })
-  
   authAxios.get(baseUrl) 
    .then((response) => {
     console.log(response.data.statuses);
-    res.send(response.data.statuses);
+    res.json(response.data.statuses[0].id);
    }) .catch((error) => {
     console.log(error)
+    res.sendStatus(500);
    })
   
 });
 
- 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 });
